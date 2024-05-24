d187cb56c6b89c289935838b752d77f66005e40a
Abhishek Nimavat <abhisheknimavat08@gmail.com>
2024-05-24 15:43:33 +0530

    add Dtos and Static roles

---
+++ JwtAuthAspNetWebAPI/Core/Dtos/UpdatePermissionDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JwtAuthAspNetWebAPI.Core.Dtos
+{
+    public class UpdatePermissionDto
+    {
+        [Required(ErrorMessage = "UserName is required")]
+        public string UserName { get; set; }
+        
+    }
+}
